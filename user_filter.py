d33392855546707e375ae95c0747cc3fd351680e
MaxBoroda <maksim290391@gmail.com>
2019-01-04 16:08:32 +0200

    Complete lesson3 all Done

--- user_filter.py
+++ user_filter.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import json
 import re
 
+
 def is_number(char):
     return char.isnumeric()
+
 
 def is_valid_phone(phone):
     pattern = re.compile('\+[0-9-]+')
@@ -12,35 +14,44 @@ def is_valid_phone(phone):
         return False
 
     clean = "".join(filter(is_number, phone))
-    if len(clean) == 14:
+    if len(clean) == 11:
         return True
     return False
 
+
 def is_valid_email(email):
-    pattern = re.compile('[a-z0-9._-]+@[a-z0-9.]+.[a-z0-9]+')
+    pattern = re.compile('[a-z0-9._-]+@[a-z0-9.]+\.[a-z0-9]+')
     if pattern.fullmatch(email.lower()):
         return True
     return False
 
-# Load DATA
-people_file='./users.json'
 
-with open(people_file) as file:
-    people_data = json.load(file)
+def run_filter(data):
+    vpd = list(filter(lambda x: is_valid_email(x['email']), data))
+    vpd = list(filter(lambda x: is_valid_phone(x['phone']), vpd))
+    vpd = list(filter(lambda x: x['age'] >= 30, vpd))
+    return vpd
 
-# Fix age field
-for person_data in people_data:
-    try:
-        age = int(person_data['age'])
-    except:
-        age = -1
-    person_data['age'] = age
 
-# Filtering
-valid_people_data = list(filter(lambda x: is_valid_email(x['email']), people_data))
-valid_people_data = list(filter(lambda x: is_valid_phone(x['phone']), valid_people_data))
-valid_people_data = list(filter(lambda x: x['age'] >= 30, valid_people_data))
+def main():
+    # Load DATA
+    people_file = 'user.json'
 
-print(json.dumps(valid_people_data, indent=4, sort_keys=True))
-\
+    with open(people_file) as file:
+        people_data = json.load(file)
 
+    # Fix age field
+    for person_data in people_data:
+        try:
+            age = int(person_data['age'])
+        except:
+            age = -1
+        person_data['age'] = age
+
+    valid_people_data = run_filter(people_data)
+
+    print(json.dumps(valid_people_data, indent=4, sort_keys=True))
+
+
+if __name__ == '__main__':
+    main()
